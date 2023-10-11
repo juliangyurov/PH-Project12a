5a4d2ff76f046e9abc71e673617b2b17e804fa7c
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-10-11 22:41:16 +0300

    Add images and renaming their names with alert controller

---
+++ Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Yulian Gyuroff on 11.10.23.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
